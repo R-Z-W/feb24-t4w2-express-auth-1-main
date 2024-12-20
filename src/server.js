e166f2ef20e7318c672f7c7a36cc6570deeb4686
R-Z-W <whiteleyzenki@gmail.com>
2024-12-21 00:11:21 +1100

    z

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 let corsOptions = {
 	origin: ["http://localhost:3000", "http://127.0.0.1:5173", "https://yourfrontenddomain.com"],
