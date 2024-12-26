a90efca3f0b1932a04c342efd1b327e630be1d61
R-Z-W <whiteleyzenki@gmail.com>
2024-12-26 23:58:25 +1100

    main server

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ let corsOptions = {
 	origin: [
 		"http://localhost:3000", 
 		"http://127.0.0.1:5173", 
-		"https://domain.com"], // Replace with domain
+		"https://lineopstest.netlify.app/src/main.jsx"], // Replace with domain
 	optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
